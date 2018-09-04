c66039e5a620f79462edc1b74c01daacb9cbebd9
Hannah Smith <hannah@highrise.digital>
2018-09-04 14:19:09 +0100

    Allow filtering of custom post type using search form

--- partials/home/searchform.php
+++ partials/home/searchform.php
@@ -6,6 +6,7 @@
             placeholder="<?php echo esc_attr_x( 'What course are you looking for?', 'placeholder' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
             title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+        <input type="hidden" name="post_type" value="courses">    
     </label>
 
     <button type="submit" class="dashicons dashicons-search" value="<?php echo esc_attr_x( 'Search', 'submit button'); ?>"></button>
