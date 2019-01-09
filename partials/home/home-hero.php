e5871f95a98dfe2490aa236be9a68ff0b8baf18b
Mahbub <mmhasaneee@gmail.com>
2019-01-09 22:15:24 +0600

    Make the search texts and black bar texts editable

--- partials/home/home-hero.php
+++ partials/home/home-hero.php
@@ -1,17 +1,46 @@
 <div class="hero-section">
-
+	<?php
+	$search_title = trim(get_field('search_title'));
+	$search_title = wp_kses($search_title, array(
+		'span' => array('class' => array(), 'style' => array()),
+		'em' => array('class' => array(), 'style' => array()),
+		'strong' => array('class' => array(), 'style' => array()),
+		'a' => array('class' => array(), 'style' => array(), 'href' => array(), 'target' => array()),
+	));
+	?>
 	<div class="center hero-overlay">
-        <h2>Training <span class="dashicons dashicons-arrow-right-alt"></span> Find Your Course</h2>
+        <?php if($search_title): ?>
+        <h2><?php echo $search_title; ?></h2>
+        <?php endif; ?>
         <?php get_template_part( 'partials/home/searchform' ); ?>
 	</div>	
 	
 </div>
 
 <div class="hero-subheading">
-
+    <?php
+    $subheading_title = trim(get_field('subheading_title'));
+    $subheading_subtitle = trim(get_field('subheading_subtitle'));
+    $subheading_title = wp_kses($subheading_title, array(
+	    'span' => array('class' => array(), 'style' => array()),
+	    'em' => array('class' => array(), 'style' => array()),
+	    'strong' => array('class' => array(), 'style' => array()),
+	    'a' => array('class' => array(), 'style' => array(), 'href' => array(), 'target' => array()),
+    ));
+    $subheading_subtitle = wp_kses($subheading_subtitle, array(
+	    'span' => array('class' => array(), 'style' => array()),
+	    'em' => array('class' => array(), 'style' => array()),
+	    'strong' => array('class' => array(), 'style' => array()),
+	    'a' => array('class' => array(), 'style' => array(), 'href' => array(), 'target' => array()),
+    ));
+    ?>
     <div class="center">
-        <h2>ALL COURSES ARE NOW AVAILABLE AT YOUR SITE</h2>
-        <p>Request a quote from an individual course page or call us on 0117 971 1892</p>
+        <?php if($subheading_title): ?>
+        <h2><?php echo $subheading_title; ?></h2>
+        <?php endif; ?>
+        <?php if($subheading_subtitle): ?>
+        <p><?php echo $subheading_subtitle; ?></p>
+        <?php endif; ?>
     </div>    
     
 </div>
--- partials/home/searchform.php
+++ partials/home/searchform.php
@@ -1,9 +1,12 @@
 <form role="search" method="get" class="search-form row" action="<?php echo home_url( '/' ); ?>">
-
+<?php
+$search_placeholder = trim(get_field('search_placeholder'));
+$search_placeholder = strip_tags($search_placeholder);
+?>
     <label>
         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
         <input type="search" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'What course are you looking for?', 'placeholder' ) ?>"
+            placeholder="<?php echo $search_placeholder; ?>"
             value="<?php echo get_search_query() ?>" name="s"
             title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
         <input type="hidden" name="post_type" value="courses"> 
